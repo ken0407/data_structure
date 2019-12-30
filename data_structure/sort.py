e0597aea75ff9b25850e00160ab67cfb88337668
keeeeen <kentaro.ishii0407@gmail.com>
2019-12-30 18:29:40 +0900

    merge sort 実装

--- data_structure/sort.py
+++ data_structure/sort.py
@@ -1,8 +1,21 @@
 # -*- coding: utf-8 -*-
 #from . import helpers
 
-def insert_sort(x:list)->list:
-    """this is quick sort algorithm."""
+def bubble_sort(x:list)->list:
+    """this is bubble sort algorithm."""
+    flag = True
+    while flag:
+        flag = False
+        for i in range(len(x)-1):
+            if x[i]>x[i+1]:
+                tmp = x[i]
+                x[i] = x[i+1]
+                x[i+1] = tmp
+                flag = True
+    return x
+
+def choose_sort(x:list)->list:
+    """this is choose sort algorithm."""
     for i in range(len(x)):
         min = x[i]
         min_idx = i
@@ -12,10 +25,34 @@ def insert_sort(x:list)->list:
                 min_idx = j
         x[min_idx] = x[i]
         x[i] = min
-    print(x)
+
     return x
 
-if __name__=='__main__':
-    x = [4, 6, -12, 200, -1432, 0]
-    x = insert_sort(x)
-    print(x)
+def merge_sort(x:list)->list:
+    def merge(left, right):
+        i = 0
+        j = 0
+        return_list = []
+        while (i < len(left)) and (j < len(right)):
+            if left[i] <= right[j]:
+                return_list.append(left[i])
+                i += 1
+            else:
+                return_list.append(right[j])
+                j += 1
+        if i == len(left)-1:
+            return_list.extend(right[j:])
+        if j == len(right)-1:
+            return_list.extend(left[i:])
+
+        return return_list
+    if len(x) <= 1:
+        return x
+
+    center = len(x) // 2
+    x_a = x[:center]
+    x_b = x[center:]
+    x_a = merge_sort(x_a)
+    x_b = merge_sort(x_b)
+
+    return merge(x_a, x_b)
--- tests/test_sort.py
+++ tests/test_sort.py
@@ -2,8 +2,20 @@ import unittest
 from .context import data_structure
 
 class TestSort(unittest.TestCase):
-    def test_insert_sort(self):
+    def test_bubble_sort(self):
+        x =[4, 6, -12, 200, -1432, 0]
+        x = data_structure.bubble_sort(x)
+        for i in range(len(x) - 1):
+            assert x[i + 1] > x[i], 'bubble sort is wrong.'
+
+    def test_choose_sort(self):
         x = [4, 6, -12, 200, -1432, 0]
-        x = data_structure.insert_sort(x)
+        x = data_structure.choose_sort(x)
         for i in range(len(x)-1):
-            assert x[i+1] > x[i], 'insert sort is wrong.'
+            assert x[i+1] > x[i], 'choose sort is wrong.'
+
+    def test_merge_sort(self):
+        x = [4, 6, -12, 200, -1432, 0]
+        x = data_structure.merge_sort(x)
+        for i in range(len(x) - 1):
+            assert x[i + 1] > x[i], 'merge sort is wrong.'
